a238e0e5d93df272ba06b34c274aeaa8d4552fd8
hany <hs1547@fayoum.edu.eg>
2025-05-18 15:14:52 +0300

    add TestFetchTeamCount add TestFetchTeamName

--- BallifyTests/BallifyTests.swift
+++ BallifyTests/BallifyTests.swift
@@ -10,28 +10,7 @@ import XCTest
 
 final class BallifyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+ 
     
     
     func testFetchLeaguesFromJSONCount(){
@@ -92,5 +71,36 @@ final class BallifyTests: XCTestCase {
         waitForExpectations(timeout: 3)
     }
     
+    
+    // test TeamCount using la liga -- Spain League
+    func testFetchTeamsFromJSONCount(){
+        let expectation = expectation(description: "Waiting for api")
+
+        NetworkService.fetchTeamsFromJSON(sportType: SportType.football.rawValue, leagueId: 302){ result in
+            if result == nil {
+                XCTFail()
+            }else {
+                XCTAssertEqual(result?.count, 20) //20
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 3)
+    }
+    
+    // test Fetch Team using la liga -- Spain League
+    func testFetchTeamsFromJSONByTeamName(){
+        let expectation = expectation(description: "Waiting for api")
+
+        NetworkService.fetchTeamsFromJSON(sportType: SportType.football.rawValue, leagueId: 302){ result in
+            if result == nil {
+                XCTFail()
+            }else {
+                XCTAssertEqual(result?[0].team_name, "Atletico Madrid")
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 3)
+    }
+
 
 }
